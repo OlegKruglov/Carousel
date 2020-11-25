9caba1712cb7538f237b7fd3a582e8a11b3cf86b
opilane <opilane@HP-CZC6297B8L>
2020-11-25 10:45:33 +0200

    Added button that opens links

--- Carousel/Carousel/MainPage.xaml.cs
+++ Carousel/Carousel/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Carousel
@@ -12,6 +13,38 @@ namespace Carousel
     {
         public MainPage()
         {
+            Button btn = new Button
+            {
+                Text="Открыть Gmail"
+            };
+            Button btn1 = new Button
+            {
+                Text="Открыть Google Maps"
+            };
+            Button btn2 = new Button
+            {
+                Text="Открыть Google Drive"
+            };
+            Button btn3 = new Button
+            {
+                Text="Открыть YouTube"
+            };
+            Button btn4 = new Button
+            {
+                Text="Открыть Google"
+            };
+            Button btn5 = new Button
+            {
+                Text="Открыть Google Translate"
+            };
+
+            btn.Clicked += Btn_Clicked;
+            btn1.Clicked += Btn1_Clicked;
+            btn2.Clicked += Btn2_Clicked;
+            btn3.Clicked += Btn3_Clicked;
+            btn4.Clicked += Btn4_Clicked;
+            btn5.Clicked += Btn5_Clicked;
+
             var gmail = new ContentPage
             {
                 Content = new StackLayout
@@ -29,8 +62,9 @@ namespace Carousel
                         WidthRequest=200,
                         HeightRequest=200,
                         HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand }
-                    }
+                        VerticalOptions=LayoutOptions.CenterAndExpand
+                    },
+                    btn }
 
                 }
             };
@@ -51,8 +85,10 @@ namespace Carousel
                         WidthRequest=200,
                         HeightRequest=200,
                         HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand }
-                    }
+                        VerticalOptions=LayoutOptions.CenterAndExpand
+                    },
+                    
+                    btn1 }
 
                 }
             };
@@ -73,8 +109,8 @@ namespace Carousel
                         WidthRequest=200,
                         HeightRequest=200,
                         HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand }
-                    }
+                        VerticalOptions=LayoutOptions.CenterAndExpand },
+                   btn2 }
 
                 }
             };
@@ -95,8 +131,8 @@ namespace Carousel
                         WidthRequest=200,
                         HeightRequest=200,
                         HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand }
-                    }
+                        VerticalOptions=LayoutOptions.CenterAndExpand },
+                    btn3}
 
                 }
             };
@@ -117,9 +153,9 @@ namespace Carousel
                         WidthRequest=200,
                         HeightRequest=200,
                         HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand }
-                    }
-
+                        VerticalOptions=LayoutOptions.CenterAndExpand },
+                    btn4}
+                    
                 }
             };
             var translate = new ContentPage
@@ -139,8 +175,8 @@ namespace Carousel
                         WidthRequest=200,
                         HeightRequest=200,
                         HorizontalOptions=LayoutOptions.Center,
-                        VerticalOptions=LayoutOptions.CenterAndExpand }
-                    }
+                        VerticalOptions=LayoutOptions.CenterAndExpand },
+                    btn5}
 
                 }
             };
@@ -151,5 +187,41 @@ namespace Carousel
             Children.Add(youtube);
             Children.Add(translate);
             }
+
+        private async void Btn5_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://translate.google.com/?hl=ru");
+            await Browser.OpenAsync(uri);
+        }
+
+        private async void Btn4_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://www.google.ru/");
+            await Browser.OpenAsync(uri);
+        }
+
+        private async void Btn3_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://www.youtube.com/");
+            await Browser.OpenAsync(uri);
+        }
+
+        private async void Btn2_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://drive.google.com/drive/u/0/my-drive");
+            await Browser.OpenAsync(uri);
+        }
+
+        private async void Btn1_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://www.google.ru/maps/@59.408384,24.7037952,12z");
+            await Browser.OpenAsync(uri);
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("https://mail.google.com/mail/u/0/?tab=wm&ogbl#inbox");
+            await Browser.OpenAsync(uri);
+        }
     }
             };
