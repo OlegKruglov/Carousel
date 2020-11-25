98a7f326e08f91b80790c7fcbc7ee955a8108ebb
opilane <opilane@HP-CZC6297B8L>
2020-11-25 11:18:17 +0200

    Icon changed

--- Carousel/Carousel.Android/MainActivity.cs
+++ Carousel/Carousel.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Carousel.Droid
 {
-    [Activity(Label = "Carousel", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Carousel", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
